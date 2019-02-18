05ea8ad421177b6b003dbb045b0a44044984e7ae
REALEYESIT\daniel.majoross <daniel.majoross@realeyesit.com>
2019-02-18 09:43:58 +0100

    hm

--- src/components/Webshop.tsx
+++ src/components/Webshop.tsx
@@ -1,32 +1,28 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
+import products, { Product } from "../utils/products";
 import NotFound from "./layout/errorPages/NotFound";
 import { Footer } from "./layout/Footer/Footer";
 import { Header } from "./layout/Header/Header";
-import { ProductList } from "./pages/products/ProductList";
-import "./Webshop.css";
-import products, { Product } from "../utils/products";
 import { Home } from "./pages/Home";
 import { ProductDetails } from "./pages/products/ProductDetails";
-interface IProps {
-  selectedProduct: Product | null
-}
-const CategoryRoutes=(props: any) => {
+import { ProductList } from "./pages/products/ProductList";
+import "./Webshop.css";
 
-}
-export class Webshop extends React.Component<IProps, {selectedProduct: Product}> {
-  constructor(props:IProps){
-    super(props)
-    this.state={
-      selectedProduct: props.selectedProduct
-    }
+const CategoryRoutes = (props: any) => {};
+export class Webshop extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      selectedProduct: {}
+    };
   }
 
-  chooseProduct(product:Product){
-    this.setState({selectedProduct: product});
-    console.log(this.state)
-    alert(this.state.selectedProduct.name)
-  }
+  chooseProduct = (product: Product) => {
+    this.setState({ selectedProduct: product });
+    console.log(this.state);
+    alert(this.state.selectedProduct.name);
+  };
 
   render() {
     return (
@@ -36,13 +32,41 @@ export class Webshop extends React.Component<IProps, {selectedProduct: Product}>
           <Switch>
             <Route exact path="/" component={Home} />
             {/* <Route path="/products" component={ProductList} /> */}
-            <Route path={"/products"} component={()=>(<ProductList selectedProduct={this.state.selectedProduct!} select={this.chooseProduct}></ProductList>)} />
-            <Route path={products.product[0].category} component={ProductList} />
-            <Route path={products.product[1].category} component={ProductList} />
-            <Route path={products.product[2].category} component={ProductList} />
-            <Route path={products.product[3].category} component={ProductList} />
-            <Route path={products.product[4].category} component={ProductList} />
-            <Route path={"/productDetail"} component={()=><ProductDetails selectedProduct={this.state.selectedProduct}></ProductDetails>}></Route>
+            <Route
+              path={"/products"}
+              component={() => (
+                <ProductList
+                  selectedProduct={this.state.selectedProduct!}
+                  select={this.chooseProduct}
+                />
+              )}
+            />
+            <Route
+              path={products.product[0].category}
+              component={ProductList}
+            />
+            <Route
+              path={products.product[1].category}
+              component={ProductList}
+            />
+            <Route
+              path={products.product[2].category}
+              component={ProductList}
+            />
+            <Route
+              path={products.product[3].category}
+              component={ProductList}
+            />
+            <Route
+              path={products.product[4].category}
+              component={ProductList}
+            />
+            <Route
+              path={"/productDetail"}
+              component={() => (
+                <ProductDetails selectedProduct={this.state.selectedProduct} />
+              )}
+            />
             <Route component={NotFound} />
           </Switch>
         </div>
